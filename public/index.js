7683140a42606a88d029093d5c18a624a7d1a2c0
Alissa <alissamak@outlook.com>
2022-04-18 23:32:52 -0400

    added winner and loser messages, submit button and cleaned up code

--- public/index.js
+++ public/index.js
@@ -1,77 +1,108 @@
-//https://stackoverflow.com/questions/8912917/cutting-an-image-into-pieces-through-javascript
-let tropicalLakeImages = [
+//winnerArray
+const tropicalLakeImages = [
     {src: 'assets/tropical_lake2-split/00.jpg', title: 'Tropical Lake 1', value: '0'},
-    {src: 'assets/tropical_lake2-split/01.jpg', title: 'Tropical Lake 2', value: '1'},
-    {src: 'assets/tropical_lake2-split/02.jpg', title: 'Tropical Lake 3', value: '2'},
-    {src: 'assets/tropical_lake2-split/03.jpg', title: 'Tropical Lake 4', value: '3'},
-    {src: 'assets/tropical_lake2-split/10.jpg', title: 'Tropical Lake 5', value: '4'},
-    {src: 'assets/tropical_lake2-split/11.jpg', title: 'Tropical Lake 6', value: '5'},
-    {src: 'assets/tropical_lake2-split/12.jpg', title: 'Tropical Lake 7', value: '6'},
-    {src: 'assets/tropical_lake2-split/13.jpg', title: 'Tropical Lake 8', value: '7'},
-    {src: 'assets/tropical_lake2-split/20.jpg', title: 'Tropical Lake 9', value: '8'},
-    {src: 'assets/tropical_lake2-split/21.jpg', title: 'Tropical Lake 10', value: '9'},
-    {src: 'assets/tropical_lake2-split/22.jpg', title: 'Tropical Lake 11', value: '10'},
+    {src: 'assets/tropical_lake2-split/10.jpg', title: 'Tropical Lake 2', value: '1'},
+    {src: 'assets/tropical_lake2-split/20.jpg', title: 'Tropical Lake 3', value: '2'},
+    {src: 'assets/tropical_lake2-split/01.jpg', title: 'Tropical Lake 4', value: '3'},
+    {src: 'assets/tropical_lake2-split/11.jpg', title: 'Tropical Lake 5', value: '4'},
+    {src: 'assets/tropical_lake2-split/21.jpg', title: 'Tropical Lake 6', value: '5'},
+    {src: 'assets/tropical_lake2-split/02.jpg', title: 'Tropical Lake 7', value: '6'},
+    {src: 'assets/tropical_lake2-split/12.jpg', title: 'Tropical Lake 8', value: '7'},
+    {src: 'assets/tropical_lake2-split/22.jpg', title: 'Tropical Lake 9', value: '8'},
+    {src: 'assets/tropical_lake2-split/03.jpg', title: 'Tropical Lake 10', value: '9'},
+    {src: 'assets/tropical_lake2-split/13.jpg', title: 'Tropical Lake 11', value: '10'},
     {src: 'assets/tropical_lake2-split/23.jpg', title: 'Tropical Lake 12', value: '11'},
-];
+]
 // console.log(tropicalLakeImages)
 
 let puzzleContainer = document.getElementsByClassName('puzzle')
+
 //looping through each lake image
 function displayImages(imageArray){
+    imageArray.forEach((element, index) =>{
+        let randomImage = parseInt(Math.random()*imageArray.length);
+        let temp = imageArray[index];
+        // console.log(index)
+        imageArray[index] = imageArray[randomImage];
+        imageArray[randomImage] = temp;
+    }) 
     imageArray.forEach((element, index) => {
         let puzzleImg = document.createElement('img')
         puzzleImg.src = element.src
         puzzleImg.setAttribute('value', element.value)
         // console.log(puzzleImg.src)
         puzzleContainer[index].innerHTML = ''
-        puzzleContainer[index].append(puzzleImg)
+        puzzleContainer[index].append(puzzleImg)  
     });
 }
 displayImages(tropicalLakeImages);
 
-//randomize images function
-function mixImages(){
-    const newMixArray = []
-    const copy = tropicalLakeImages;
-    console.log(copy)
-    while(copy.length){
-        let randomIndex = Math.floor(Math.random()*(copy.length-1))
-        let randomSplicer = copy.splice(randomIndex, 1)
-        console.log(randomSplicer[0])
-        newMixArray.push(randomSplicer[0])
-    }
-    
-    displayImages(newMixArray);
-    
+//refresh button to randomize and start over function
+// const copyMixArray = tropicalLakeImages;
+function mixImages(copyMixArray){
+    copyMixArray = tropicalLakeImages;
+    copyMixArray.forEach((element, index) =>{
+        let randomImage = parseInt(Math.random()*copyMixArray.length);
+        let temp = copyMixArray[index];
+        // console.log(index)
+        copyMixArray[index] = copyMixArray[randomImage];
+        copyMixArray[randomImage] = temp;
+    }) 
+    // for(let i = 0; i < copyMixArray.length; i++) {
+    //     // for each index,i pick a random index j 
+    //     let randomImage = parseInt(Math.random()*copyMixArray.length);
+    //     // swap elements at i and j
+    //     let temp = copyMixArray[i];
+    //     copyMixArray[i] = copyMixArray[randomImage];
+    //     copyMixArray[randomImage] = temp;
+    // }   
+    // console.log(copyMixArray);
+    displayImages(copyMixArray);
 }
-
+//initating randomizing button
 document.getElementById('refresh-btn').addEventListener('click', mixImages)
 
-//correct image function
-// image element
-// parent element
-function solvedImage(imageElement, parentElement){
-    imageElement.every((element, index) => {
-        console.log(element)
-        return element.value === parentElement.value
-    })
+//winner message function
+function displayWinner(){
+    let winnerMessage = "Congrats you solved the puzzle!"
+    console.log(winnerMessage)
+    window.alert("Congrats you solved the puzzle!")
 }
-// solvedImage(tropicalLakeImages)
-// const winner = (imageElement, parentElement) => imageElement.value === parentElement.value
-// console.log(tropicalLakeImages.every(winner))
+//loser message function
+function displayLoser(){
+    let winnerMessage = "Sorry you didn't quite solve the puzzle. Try again!"
+    console.log(winnerMessage)
+    window.alert("Sorry you didn't quite solve the puzzle. Try again!")
+}
 
-let winnerMessage = document.createElement('div')
-winnerMessage.setAttribute('id', 'winner-message')
-winnerMessage.innerHTML = "Congrats you solved the puzzle!"
+//correct image function, need image and parent element
+// function solvedImage(imageElement, parentElement){
+//     imageElement.every((element, index) => {
+//         console.log(element)
+//         return imageElement.value === parentElement.value
+//     })
+// }
+let currentArray = [];
+function solvedImage(){
+    const correctImage = (currentArray) => currentArray === tropicalLakeImages;
+    console.log(tropicalLakeImages.every(correctImage))
+    if(currentArray === tropicalLakeImages){
+        displayWinner()
+    }
+    else{
+        displayLoser()
+    }
+}
+//initating correct image function
+document.getElementById('submit-btn').addEventListener('click', solvedImage)
 
-//https://web.dev/drag-and-drop/
-//drag and drop functions
+//drag and drop functions. Source: https://web.dev/drag-and-drop/
 document.addEventListener('DOMContentLoaded', (event) => {
-
+    // let draggedArray =[];
     function handleDragStart(e) {
         // this.style.opacity = '0.4';
         dragSrcEl = this;
-      
+        // console.log(dragSrcEl);
         e.dataTransfer.effectAllowed = 'move';
         e.dataTransfer.setData('text/html', this.innerHTML);
     }
@@ -97,7 +128,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
   
     let items = document.querySelectorAll('.puzzle');
-    items.forEach(item => {
+    items.forEach((item, index) => {
       item.addEventListener('dragstart', handleDragStart);
       item.addEventListener('dragover', handleDragOver);
       item.addEventListener('dragenter', handleDragEnter);
@@ -105,6 +136,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       item.addEventListener('dragend', handleDragEnd);
       item.addEventListener('drop', handleDrop);
     });
+    
   });
 
   function handleDrop(e) {
@@ -117,29 +149,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
   
     return false;
   }
- 
-//correct image function
+// console.log(draggedArray)
 
-// const isSolvable = (arr) => {
-//     let number_of_inv = 0;
-//     // get the number of inversions
-//     for(let i =0; i<arr.length; i++){
-//         // i picks the first element
-//         for(let j = i+1; j < arr.length; j++) {
-//             // check that an element exist and index i and j, then check that element at i > at j
-//             if((arr[i] && arr[j]) && arr[i] > arr[j]) number_of_inv++;
-//         }
-//     }
-//     // if the number of inversions is even
-//     // the puzzle is solvable
-//     return (number_of_inv % 2 == 0);
-// }
+// function isSolvable(arr){
+//       arr = tropicalLakeImages
+//       let number_of_inv = 0;
+//       // get the number of inversions
+//       for(let i =0; i<arr.length; i++){
+//           // i picks the first element
+//           for(let j = i+1; j <arr.length; j++) {
+//               // check that an element exist and index i and j, then check that element at i > at j
+//               if((arr[i] && arr[j]) && arr[i] > arr[j]) number_of_inv++;
+//           }
+//       }
+//       // if the number of inversions is even
+//       // the puzzle is solvable
+//       console.log(number_of_inv)
+//       return (number_of_inv % 2 == 0);
+//   } 
+// // isSolvable(draggedArray)
 
-//you win message after correct image is complete
-
-
-//next button for new image function
-
-
-//refresh button to start over function
-
+// function isCorrect (solution, content) {
+//       if(JSON.stringify(solution) == JSON.stringify(content)) return true;
+//       return false;
+//   }
+//optional: next button for new image function
